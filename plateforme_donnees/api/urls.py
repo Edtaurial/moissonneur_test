25dd27d4f1f640a6c24936473d4f401f7fd2c98d
Princial Nzeguim <princialnzeguim15@gmail.com>
2025-12-07 14:35:34 -0500

    Update API paths to remove 'api/' prefix

--- plateforme_donnees/api/urls.py
+++ plateforme_donnees/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import JeuDeDonneesListAPIView, JeuDeDonneesDetailAPIView
 from .views import ManageUserView, RegisterView
 urlpatterns = [
-    path('api/donnees/', JeuDeDonneesListAPIView.as_view(), name='donnees-list'),
-    path('api/donnees/<int:pk>/', JeuDeDonneesDetailAPIView.as_view(), name='donnees-detail'),
+    path('/donnees/', JeuDeDonneesListAPIView.as_view(), name='donnees-list'),
+    path('/donnees/<int:pk>/', JeuDeDonneesDetailAPIView.as_view(), name='donnees-detail'),
     path('api/me/', ManageUserView.as_view(), name='user-profile'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
